45b53a9e9e5a935d3716bb530643f1b3b70decf4
Facundo <facugastonbarral@gmail.com>
2022-05-19 19:01:13 -0300

    Add auth controller with verifications

---
+++ controllers/authController.js
@@ -0,0 +1,19 @@
+var express = require('express');
+const { User } = require('../models/index')
+const bcrypt = require('bcrypt')
+
+exports.login = async function( req, res ){
+    var {email, password} = req.body
+    
+    // Check if user exists
+    const userExist = await User.findOne({ where: { email: email } });
+    if (!userExist) return res.status(404).send({ok: false})
+    
+    //Decrypt and check password 
+    bcrypt.compare(password, userExist.password, function(err, result) {
+        if (!result) return res.status(401).send({ok: false})
+    });
+
+    res.send(userExist)
+
+}
