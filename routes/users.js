5bc37ce51937942100f85a3544e411ea515e3ed5
Santiago Raab <Santiagoraab@gmail.com>
2022-05-22 16:40:28 -0300

    POST user authenticate and password encrypted

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+const validateCreate = require('../controllers/userValidator');
+const db = require('../models/index')
+const { User } = db;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/auth/register', validateCreate,
+async (req, res) => {
+  let passwordHash = await bcrypt.hash(req.body.password, 10)
+  User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: passwordHash
+  })
+  .then(user =>  res.json(user))
+})
 
 module.exports = router;
