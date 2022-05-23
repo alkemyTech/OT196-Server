2a79e32bd8a77197e114bf2d387053a58f9f6563
Coty Paradelo <cotyparadelo1@gmail.com>
2022-05-23 16:24:40 -0300

    Endpoint authenticated user data

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// GET logged user data
+
+router.get('/auth/me', function(req, res) {
+  let token = req.header.token;
+  user.find({ token: token }, function(err, userMe) {
+      if (err) {
+          return res.json({
+              success: false,
+              msj: 'No se encontró ningún usuario',
+              err
+          });
+      } else {
+          return res.json({
+              success: true,
+              user: userMe
+          });
+      }
+  })
+});
+
+
+
+
 module.exports = router;
