9decb521b05d4a34785ebb0f58de6d90caaecdf1
Facundo <facugastonbarral@gmail.com>
2022-05-27 18:01:04 -0300

    Fix on token data invalid check

--- middlewares/validators/userValidator.js
+++ middlewares/validators/userValidator.js
@@ -3,14 +3,13 @@ const jwt = require('jsonwebtoken');
 const adminRoles = [1]
 
 exports.roleVerify = async function(req,res,next){
-    const reqToken = req.headers.authorization.replace("Bearer ", "");
+    const reqToken = req.headers.authorization.split(' ')[1];
     const token = jwt.verify(reqToken, JWT_SECRET, function (err, suc) {
         if (err) {return res.status(401).end('Invalid token.');}
         return suc
     })
     const role = token.roleId 
-    if (!role) return res.status(400).end('Token data invalid.')
+    if (role === undefined) return res.status(400).end('Token data invalid.')
     if (!adminRoles.includes(role)) return res.status(403).end('User role unauthorized')
-    
     next()
 }
