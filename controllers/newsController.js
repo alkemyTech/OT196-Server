a880bd73284d157e315913349f67f22e1783f222
Facundo <facugastonbarral@gmail.com>
2022-05-30 20:31:52 -0300

    Added error catch to entry creation

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,8 +1,10 @@
 const { Entry } = require('../models/index')
 
 exports.createNews = async function( req, res ){
+    // Admin role & neccessary values are already validated
     const {name, content, image, categoryId} = req.body
-    const type = "news"
-    const createdNew = await Entry.create({ name, content, image, categoryId, type})
-    res.status(200).send({success: true, message: "News created successfully.", createdNew: createdNew})
+    const type = "news" 
+    Entry.create({ name, content, image, categoryId, type})
+    .then(newEntry => res.status(200).send({success: true, message: "News created successfully.", createdNew: newEntry}))
+    .catch(e => res.status(400).send({success: false, message: e.errors[0].message || e.name}))
 }
