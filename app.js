64812ed6c80441e2104300d9b9c55cee3576e4b0
Facundo <facugastonbarral@gmail.com>
2022-05-31 00:28:22 -0300

    Added news router to app

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
 const testimonialsRouter = require("./routes/testimonials");
+const newsRouter = require('./routes/news');
 
 const app = express();
 app.use(cors());
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 app.use("/organizations", testimonialsRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
