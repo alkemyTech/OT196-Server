5e17e6b8e8605a3605cf100174c5bc2566ce004e
Facundo <facugastonbarral@gmail.com>
2022-06-02 13:44:52 -0300

    Created activities route

---
+++ routes/activities.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const db = require("../models/index");
+const { updateActivity } = require("../controllers/activitiesController");
+const { adminValidation } = require('../middlewares/validators/userValidators');
+const { validateActivity } = require("../middlewares/validators/formsValidator")
+
+router.put('/', adminValidation, validateActivity, updateActivity)
