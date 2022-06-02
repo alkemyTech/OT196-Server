353297e83664f3080a541afe4610f9767c0ea62a
Facundo <facugastonbarral@gmail.com>
2022-06-02 12:27:50 -0300

    Created controller for activities

---
+++ controllers/activitiesController.js
@@ -0,0 +1,23 @@
+const { Activity } = db;
+
+exports.updateActivity = async function(req, res){
+    const { id } = req.params
+    const { name, image, content } = req.body
+
+    //Check if Activity exists
+    updActivity = Activity.findByPk(id)
+    if (!updActivity) return res.status(404).send({ok: false, message: 'Activity not found.'})
+
+    try{
+        await updActivity.update({
+            name: name,
+            image: image,
+            content: content,
+        });
+    }
+    catch(e){
+        if (!updActivity) return res.status(404).send({ok: false, message: 'Error updating activity.'})
+    }
+
+    if (!updActivity) return res.status(200).send({ok: true, message: 'Activity updated.'})
+}
