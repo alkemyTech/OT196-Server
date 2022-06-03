79fb7fd4e470230483722956002ce53700d36924
Facundo <facugastonbarral@gmail.com>
2022-06-02 23:25:07 -0300

    Remove admin validation

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,6 +4,6 @@ const { adminValidation } = require('../middlewares/validators/userValidators');
 const { validateContact } = require("../middlewares/validators/formsValidator");
 const { createContact } = require('../controllers/contactsController');
 
-router.post('/', adminValidation, validateContact, createContact)
+router.post('/', validateContact, createContact)
 
 module.exports = router;
