4dc82f07c7e61847c3f80a9df993dad5e50bfc0e
Facundo <facugastonbarral@gmail.com>
2022-06-02 23:44:52 -0300

    Remove unnecessary import

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { adminValidation } = require('../middlewares/validators/userValidators');
 const { validateContact } = require("../middlewares/validators/formsValidator");
 const { createContact } = require('../controllers/contactsController');
 
