2fb48e31f5569b3700d20b0fec31b52c473569b6
Facundo <facugastonbarral@gmail.com>
2022-06-05 18:00:49 -0300

    Create new testimony table

---
+++ migrations/20220605202050-update-testimony.js
@@ -0,0 +1,33 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Testimonials", {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        name: {
+          type: Sequelize.STRING,
+        },
+        content: {
+          type: Sequelize.STRING(2000),
+        },  
+        image: {
+          type: Sequelize.TEXT('long'),
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Testimonials");
+  },
+};
