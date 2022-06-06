6ce1963d712de1f52e377e4ad41cd0a9f94fb936
Ezequiel Gonzalez <zeque.mdq.95@gmail.com>
2022-06-06 13:09:55 -0300

    Added the use of the email sending service in the Contacts registration

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,21 +1,32 @@
-const { Contact } = require('../models/index')
+const { sendEmail } = require("../utils/emailSender");
+const { Contact } = require("../models/index");
 
-exports.createContact = async function(req, res){
-    const { name, email, phone, message } = req.body
-    // Trying create new contact
-    try{
-        const newContact = await Contact.create({
-            name: name,
-            email: email,
-            phone: phone || "",
-            message: message || ""
-        })
+exports.createContact = async function (req, res) {
+  const { name, email, phone, message } = req.body;
+  // Trying create new contact
+  try {
+    const newContact = await Contact.create({
+      name: name,
+      email: email,
+      phone: phone || "",
+      message: message || "",
+    });
 
-        // Response on success request
-        if (newContact) return res.status(200).send({ok: true, message: 'Contact created.', newContact: newContact})
+    // Response on success request
+    if (newContact) {
+      sendEmail({ email, name }, "contact");
+      return res.status(200).send({
+        ok: true,
+        message: "Contact created.",
+        newContact: newContact,
+      });
     }
-    catch(e){
-        // Response with error on fail request
-        return res.status(400).send({ok: false, message: 'Error creating contact.', error: e.parent?.sqlMessage || e.name})
-    }
-}
+  } catch (e) {
+    // Response with error on fail request
+    return res.status(400).send({
+      ok: false,
+      message: "Error creating contact.",
+      error: e.parent?.sqlMessage || e.name,
+    });
+  }
+};
