f671be4aa42aa34077faf0bed4cec196987f08e9
Facundo <facugastonbarral@gmail.com>
2022-06-06 23:05:57 -0300

    Using update first

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -4,20 +4,12 @@ exports.updateActivity = async function(req, res){
     const { id } = req.params
     const { name, image, content } = req.body
 
-    //Check if Activity exists
-    updActivity = Activity.findByPk(id)
-    if (!updActivity) return res.status(404).send({ok: false, message: 'Activity not found.'})
-
-    try{
-        await updActivity.update({
-            name: name,
-            image: image,
-            content: content,
-        });
-    }
-    catch(e){
-        if (!updActivity) return res.status(404).send({ok: false, message: 'Error updating activity.'})
-    }
-
-    if (!updActivity) return res.status(200).send({ok: true, message: 'Activity updated.'})
+    await Activity.update({name: name, image: image, content: content}, {where: {id: id}, returning: true})
+    .then(async () => {
+        // Get result after success update.
+        const updatedActivity = await Activity.findByPk(id) 
+        res.status(200).send({ok: true, message: 'Activity updated.', newData: updatedActivity})
+    })
+    .catch((e) => res.status(500).send({ok: false, message: 'Cant update activity.', error: e.original?.sqlMessage || e.name || "Error on update."}))
+    
 }
