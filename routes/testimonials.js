6176dcd87742b9d9c149b57fda5b2ea02b41528b
Facundo <facugastonbarral@gmail.com>
2022-06-08 14:44:46 -0300

    Remove endpoint that corresponds to organizations

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -7,33 +7,6 @@ const db = require("../models/index");
 // Add Testimony's Model
 const { Testimony } = db;
 
-// GET all Testimonials' information
-router.get("/1/public", async (req, res, next) => {
-  try {
-    // Get data from DB
-    const allTestimonials = await Testimony.findAll({
-      attributes: [
-        "firstName",
-        "lastName",
-        "image",
-        "phone",
-        "address",
-        "welcomeText",
-      ],
-    });
-
-    // Name will be the union between firstName and lastName
-    allTestimonials[0].dataValues.name =
-      allTestimonials[0].dataValues.firstName +
-      " " +
-      allTestimonials[0].dataValues.lastName;
-
-    res.json(allTestimonials);
-  } catch (e) {
-    return res.status(500).json({ message: e.message });
-  }
-});
-
 router.post('/', validateTest, (req, res) => {
   try {
     Testimony.create({
