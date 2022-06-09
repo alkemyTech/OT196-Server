ab5a459353d2ef3d4c2670e7fa5d7d4a516de578
Ezequiel Gonzalez <zeque.mdq.95@gmail.com>
2022-06-09 18:48:09 -0300

    New endpoint: DELETE /testimonials/:id

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -2,11 +2,14 @@ var express = require("express");
 const {
   getAllTestimonials,
   createTestimony,
+  deleteTestimony,
 } = require("../controllers/testimonialsController");
 var router = express.Router();
 const validateTest = require("../controllers/validateTestimony");
+const { adminValidation } = require("../middlewares/validators/userValidators");
 
 router.get("/1/public", getAllTestimonials);
 router.post("/", validateTest, createTestimony);
+router.delete("/:id", adminValidation, deleteTestimony);
 
 module.exports = router;
