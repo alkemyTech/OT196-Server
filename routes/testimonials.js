643d6a6a4562ec5863f4edf4d01669a7a68c9e84
Ezequiel Gonzalez <zeque.mdq.95@gmail.com>
2022-06-09 18:45:17 -0300

    Reorganization of testimonials' routes

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,49 +1,12 @@
 var express = require("express");
-const validateTest = require("../controllers/validateTestimony");
+const {
+  getAllTestimonials,
+  createTestimony,
+} = require("../controllers/testimonialsController");
 var router = express.Router();
+const validateTest = require("../controllers/validateTestimony");
 
-const db = require("../models/index");
-
-// Add Testimony's Model
-const { Testimony } = db;
-
-// GET all Testimonials' information
-router.get("/1/public", async (req, res, next) => {
-  try {
-    // Get data from DB
-    const allTestimonials = await Testimony.findAll({
-      attributes: [
-        "firstName",
-        "lastName",
-        "image",
-        "phone",
-        "address",
-        "welcomeText",
-      ],
-    });
-
-    // Name will be the union between firstName and lastName
-    allTestimonials[0].dataValues.name =
-      allTestimonials[0].dataValues.firstName +
-      " " +
-      allTestimonials[0].dataValues.lastName;
-
-    res.json(allTestimonials);
-  } catch (e) {
-    return res.status(500).json({ message: e.message });
-  }
-});
-
-router.post('/', validateTest, (req, res) => {
-  try {
-    Testimony.create({
-      name: req.body.name,
-      content: req.body.content
-    })
-    .then(result =>  res.json(result))
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-})
+router.get("/1/public", getAllTestimonials);
+router.post("/", validateTest, createTestimony);
 
 module.exports = router;
