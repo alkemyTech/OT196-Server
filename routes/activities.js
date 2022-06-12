061aaa73d68a295b13052d41ebfa77bdd6dc1b2b
gerardodelascuevas <gerdlcp@gmail.com>
2022-06-12 12:09:59 -0500

    update route in activities

--- routes/activities.js
+++ routes/activities.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { updateActivity, getActivityDetails } = require("../controllers/activitiesController");
 const { adminValidation } = require('../middlewares/validators/userValidators');
-const { validateActivity, validateNewActivity } = require("../middlewares/validators/formsValidator");
+const { validateActivity  } = require("../middlewares/validators/formsValidator");
+const { validateNewActivity } = require('../middlewares/validators/activityValidator')
 const { createActivity } = require("../controllers/activitiesController");
 
 router.put('/:id', express.json({limit: "2mb"}), validateActivity, adminValidation, updateActivity)
@@ -13,12 +14,12 @@ router.use(express.urlencoded({extended: false}))
 //More endpoints with default limit
 
 router.get("/:id", getActivityDetails);
-router.post("/", adminValidation, validateActivity, createActivity); //RUTA MODIFICADA, VALIDATENEWACTIVITIE NO ESTA EN FORMSVALIDATOR
+router.post("/", adminValidation, validateNewActivity, createActivity); //RUTA MODIFICADA, VALIDATENEWACTIVITIE NO ESTA EN FORMSVALIDATOR
 
 module.exports = router;
 
 
 router.get("/:id", getActivityDetails);
-router.post("/", adminValidation, validateActivity, createActivity);//RUTA MODIFICADA, VALIDATENEWACTIVITIE NO ESTA EN FORMSVALIDATOR
+router.post("/", adminValidation, validateNewActivity, createActivity);//RUTA MODIFICADA, VALIDATENEWACTIVITIE NO ESTA EN FORMSVALIDATOR
 
 module.exports = router;
