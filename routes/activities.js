e6fa6fcdec675d588173b5073c37e79a0765ac85
gerardodelascuevas <gerdlcp@gmail.com>
2022-06-12 12:11:53 -0500

    deleting useless comments

--- routes/activities.js
+++ routes/activities.js
@@ -14,12 +14,12 @@ router.use(express.urlencoded({extended: false}))
 //More endpoints with default limit
 
 router.get("/:id", getActivityDetails);
-router.post("/", adminValidation, validateNewActivity, createActivity); //RUTA MODIFICADA, VALIDATENEWACTIVITIE NO ESTA EN FORMSVALIDATOR
+router.post("/", adminValidation, validateNewActivity, createActivity); 
 
 module.exports = router;
 
 
 router.get("/:id", getActivityDetails);
-router.post("/", adminValidation, validateNewActivity, createActivity);//RUTA MODIFICADA, VALIDATENEWACTIVITIE NO ESTA EN FORMSVALIDATOR
+router.post("/", adminValidation, validateNewActivity, createActivity);
 
 module.exports = router;
