ffc3a213778f0d3f020a3391e05f6ee59a8758e8
Facundo <facugastonbarral@gmail.com>
2022-06-13 00:31:30 -0300

    Add get endpoint

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const { adminValidation } = require("../middlewares/validators/userValidators");
-const { updateCategory } = require("../controllers/categoriesController");
+const { updateCategory, getAllCategories } = require("../controllers/categoriesController");
 
 router.put("/:id", adminValidation, updateCategory);
+router.get("/", getAllCategories);
 
 module.exports = router;
