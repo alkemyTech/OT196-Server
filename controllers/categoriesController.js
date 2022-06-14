275fce19a4b1807be8511d0d40903e94faa1d9ef
Facundo <facugastonbarral@gmail.com>
2022-06-14 20:07:00 -0300

    Fix function format (prettified)

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -36,27 +36,40 @@ exports.updateCategory = async (req, res) => {
 };
 
 exports.getAllCategories = async (req, res) => {
-  await Category.findAll({attributes: ['name', 'id']})
-  .then((response)=>res.status(200).send({success: true, result: response}))
-  .catch((e)=>res.status(500).send({success: false, result: e.original?.sqlMessage || e.name || "Error on fetch categories list."}))
-}
+  await Category.findAll({ attributes: ["name", "id"] })
+    .then((response) =>
+      res.status(200).send({ success: true, result: response })
+    )
+    .catch((e) =>
+      res
+        .status(500)
+        .send({
+          success: false,
+          result:
+            e.original?.sqlMessage ||
+            e.name ||
+            "Error on fetch categories list.",
+        })
+    );
+};
 
 exports.deleteCategory = async (req, res) => {
-    try {
-        await Category.destroy({ where: { id: req.params.id }, returning: true})
-        .then(
-            rowsDestroyed => rowsDestroyed ? 
-            res.status(204).send('Deleted category successfull!') 
-            : 
-            res.status(404).send({ 
-                success: false, 
-                message: 'This category is not in the list' 
-            })
-        )
-    } catch (error) {
-        res.status(500).send({ 
+  try {
+    await Category.destroy({
+      where: { id: req.params.id },
+      returning: true,
+    }).then((rowsDestroyed) =>
+      rowsDestroyed
+        ? res.status(204).send("Deleted category successfull!")
+        : res.status(404).send({
             success: false,
-            message: error.message
-        })
-    }
-}
+            message: "This category is not in the list",
+          })
+    );
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: error.message,
+    });
+  }
+};
