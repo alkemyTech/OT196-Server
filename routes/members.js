cffcb5ce6a1b90feccf1bb5dad0877db16c6c7f4
gerardodelascuevas <gerdlcp@gmail.com>
2022-06-17 13:44:59 -0500

    route and controller for update a member ready

--- routes/members.js
+++ routes/members.js
@@ -4,5 +4,5 @@ const { validateMember } = require('../middlewares/validators/membersValidator')
 const { createMember, updateMember } = require('../controllers/membersController')
 
 router.post('/', validateMember , createMember);
-router.put('/:id', validateMember, updateMember)
+router.put('/:id', validateMember, updateMember);
 module.exports = router;
