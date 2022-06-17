8db2fedcc96ff98434cedcfc6f76e6ad3f6223f2
Santiago Raab <Santiagoraab@gmail.com>
2022-06-17 02:18:40 -0300

    Solving conflicts

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -19,3 +19,21 @@ exports.getMembers = async(req, res) => {
         })
     }
 }
+
+exports.createMember = async(req, res) => {
+    try {
+        await members.create({
+            name: req.body.name,
+            image: req.body.image || ''
+        })
+        res.status(200).json({
+            success: true,
+            message: '¡Nuevo miembro creado con éxito!'
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
