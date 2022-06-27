9c0684bda29c2b0c1e66a6a0c84423c35412d985
Facundo <facugastonbarral@gmail.com>
2022-06-27 00:44:11 -0300

    Use try on upload object

--- utils/uploadFileS3.js
+++ utils/uploadFileS3.js
@@ -27,14 +27,14 @@ var s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
 // Upload a file to the bucket. It returns a string with the file's key.
 const uploadFile = async (fileData, fileName) => {
-  const file = Buffer.from(fileData.replace(/^data:image\/\w+;base64,/, ""),'base64')
-  const params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
-    Key: `images/${randomKeyGenerator()}-${fileName}`,
-    ContentEncoding: 'base64',
-    Body: file,
-  };
   try {
+    const file = Buffer.from(fileData.replace(/^data:image\/\w+;base64,/, ""),'base64')
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `images/${randomKeyGenerator()}-${fileName}`,
+      ContentEncoding: 'base64',
+      Body: file,
+    };
     const stored = await s3.upload(params).promise();
     return stored;
   } catch (e) {
