5a6a48987311ee1478aba5cbdc57ae0484ac72e0
Facundo <facugastonbarral@gmail.com>
2022-06-28 17:25:31 -0300

    Endpoint post images

--- routes/images.js
+++ routes/images.js
@@ -4,6 +4,6 @@ const { uploadImage } = require("../controllers/imagesController");
 const { adminValidation } = require("../middlewares/validators/userValidators");
 
 
-router.post("/", express.json({limit: "2mb"}), adminValidation, uploadImage);
+router.post("/", express.json({limit: "2mb"}), uploadImage);
 
 module.exports = router;
