aca7530497c80b265ec879030cb0c35f1d337325
Facundo <facugastonbarral@gmail.com>
2022-06-28 17:25:21 -0300

    Add update & delete logged user endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,16 @@
 var express = require("express");
 var router = express.Router();
 const AuthController = require("../controllers/authController");
-const { validateLogin } = require("../middlewares/validators/formsValidator");
-const { getLoggedUser } = require("../controllers/authController");
+const { validateLogin, validateUpdateUser } = require("../middlewares/validators/formsValidator");
+const { getLoggedUser, updateLoggedUser } = require("../controllers/authController");
+const { loggedValidation } = require("../middlewares/validators/userValidators");
 
 // GET logged user data
 router.get("/me", getLoggedUser);
+// UPDATE logged user data
+router.put("/me", loggedValidation, validateUpdateUser, updateLoggedUser);
+// DELETE logged user 
+router.delete("/me", loggedValidation, updateLoggedUser);
 
 router.post("/login/", validateLogin, AuthController.login);
 
