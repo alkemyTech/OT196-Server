44d299e95f433a0dbfc9efd1b14a2466e190eda8
Facundo <facugastonbarral@gmail.com>
2022-06-28 18:09:21 -0300

    Fix function on delete endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const AuthController = require("../controllers/authController");
 const { validateLogin, validateUpdateUser } = require("../middlewares/validators/formsValidator");
-const { getLoggedUser, updateLoggedUser } = require("../controllers/authController");
+const { getLoggedUser, updateLoggedUser, deleteLoggedUser } = require("../controllers/authController");
 const { loggedValidation } = require("../middlewares/validators/userValidators");
 
 // GET logged user data
@@ -10,7 +10,7 @@ router.get("/me", getLoggedUser);
 // UPDATE logged user data
 router.put("/me", loggedValidation, validateUpdateUser, updateLoggedUser);
 // DELETE logged user 
-router.delete("/me", loggedValidation, updateLoggedUser);
+router.delete("/me", loggedValidation, deleteLoggedUser);
 
 router.post("/login/", validateLogin, AuthController.login);
 
