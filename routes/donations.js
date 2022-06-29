9c79b390b0895a9974602d7c7cea051021cd33c5
gerardodelascuevas <gerdlcp@gmail.com>
2022-06-28 21:43:38 -0500

    delete console log

--- routes/donations.js
+++ routes/donations.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', (req, res)=> {
- console.log(req.body)   
+router.post('/', (req, res)=> {  
  if(req.body.donation){
     res.send({msg: true})
  }
-
 })
 
 module.exports = router;
