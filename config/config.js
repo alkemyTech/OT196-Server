577770c103218108edfe982c98f1ac1e2b604d54
ezequielmgonzalez <e.gonzalez95@hotmail.com>
2022-07-01 18:19:41 -0300

    Modified scripts and config to set up the tests

--- config/config.js
+++ config/config.js
@@ -1,29 +1,29 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
-    "development": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database":  process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "port": process.env.DB_PORT,
-        "secretToken": process.env.DB_TOKEN,
-        "dialect": "mysql"
-    },
-    "test": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "secretToken": process.env.DB_TOKEN,
-        "dialect": "mysql"
-    },
-    "production": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "secretToken": process.env.DB_TOKEN,
-        "dialect": "mysql"
-    }
-}
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    secretToken: process.env.DB_TOKEN,
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    secretToken: process.env.DB_TOKEN,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    secretToken: process.env.DB_TOKEN,
+    dialect: "mysql",
+  },
+};
