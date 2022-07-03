bd173fb037a49e390d66cbf6782a6b9590a32747
ezequielmgonzalez <e.gonzalez95@hotmail.com>
2022-07-02 22:32:54 -0300

    Added tests for PUT /users/:id

--- tests/users.test.js
+++ tests/users.test.js
@@ -131,3 +131,90 @@ describe("DELETE /users/:id", () => {
     });
   });
 });
+
+describe("PUT /users/:id", () => {
+  test("Fails: user is not logged in", async () => {
+    await request(app).put("/users/1").expect(401);
+  }, 100000);
+
+  describe("If user is logged in", () => {
+    test("Fails: user is not an admin", async () => {
+      const loggedUser = await request(app)
+        .post("/auth/login")
+        .send({
+          email: testUser.email,
+          password: testUser.password,
+        })
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+
+      await request(app)
+        .delete(`/users/1`)
+        .set("Authorization", `Bearer ${loggedUser.body.user.token}`)
+        .expect(403);
+    }, 100000);
+
+    describe("If user is admin", () => {
+      test("Fails: id is not valid", async () => {
+        const invalidId = "12jhhj31231";
+
+        const loggedUser = await request(app)
+          .post("/auth/login")
+          .send({
+            email: testAdmin.email,
+            password: testAdmin.password,
+          })
+          .expect(200)
+          .expect("Content-Type", /application\/json/);
+
+        await request(app)
+          .delete(`/users/${invalidId}`)
+          .set("Authorization", `Bearer ${loggedUser.body.user.token}`)
+          .expect(403);
+      }, 100000);
+
+      test("Success: id is valid", async () => {
+        const testNewUser = {
+          firstName: "Will Be",
+          lastName: "Updated",
+          email: "willbeupdated@mail.com",
+          password: "willbeupdated",
+          roleId: 2,
+        };
+
+        const updates = {
+          firstName: "Updated",
+          lastName: "User",
+          email: "updated@mail.com",
+          password: "updated",
+        };
+
+        await request(app)
+          .post("/users/auth/register")
+          .send(testNewUser)
+          .expect(200)
+          .expect("Content-Type", /application\/json/);
+
+        const loggedUser = await request(app)
+          .post("/auth/login")
+          .send({
+            email: testAdmin.email,
+            password: testAdmin.password,
+          })
+          .expect(200)
+          .expect("Content-Type", /application\/json/);
+
+        const userToUpdate = await User.findOne({
+          where: { email: testNewUser.email },
+        });
+
+        await request(app)
+          .delete(`/users/${userToUpdate.id}`)
+          .set("Authorization", `Bearer ${loggedUser.body.user.token}`)
+          .send(updates)
+          .expect(200)
+          .expect("Content-Type", /application\/json/);
+      }, 100000);
+    });
+  });
+});
