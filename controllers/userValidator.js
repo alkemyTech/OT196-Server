fe3bde2e457c3b78085b3d374a60d0250cbd03c1
ezequielmgonzalez <e.gonzalez95@hotmail.com>
2022-07-02 22:29:54 -0300

    Added id validator for users

--- controllers/userValidator.js
+++ controllers/userValidator.js
@@ -1,30 +1,32 @@
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require("express-validator");
 
 const validateCreate = [
-    check('firstName')
-      .exists()
-      .not()
-      .isEmpty(),
-    check('lastName')
-      .exists()
-      .not()
-      .isEmpty(),
-    check('email')
-      .exists()
-      .isEmail(),
-    check('password')
-      .exists()
-      .not()
-      .isEmpty(),
-    (req, res, next) => {
-        try {
-            validationResult(req).throw()
-            return next()
-        } catch (err) {
-            res.status(403)
-            res.send({ errors: err.array() })
-        }
+  check("firstName").exists().not().isEmpty(),
+  check("lastName").exists().not().isEmpty(),
+  check("email").exists().isEmail(),
+  check("password").exists().not().isEmpty(),
+  (req, res, next) => {
+    try {
+      validationResult(req).throw();
+      return next();
+    } catch (err) {
+      res.status(403);
+      res.send({ errors: err.array() });
     }
-]
+  },
+];
 
-module.exports = validateCreate;
+const validateId = [
+  check("id").exists().not().isEmpty().isInt(),
+  (req, res, next) => {
+    try {
+      validationResult(req).throw();
+      return next();
+    } catch (err) {
+      res.status(403);
+      res.send({ errors: err.array() });
+    }
+  },
+];
+
+module.exports = { validateCreate, validateId };
